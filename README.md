3a7f40c1bfb924a9925e254be8dc412dd44a3ca1
Gryphon Shafer <gryphon@goldenguru.com>
2016-11-09 11:53:19 -0800

    Initial commit for v1.01

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# Bot-IRC-X-WwwShorten
-Bot::IRC plugin for automatic URL shortening
+# NAME
+
+Bot::IRC::X::WwwShorten - Bot::IRC plugin for automatic URL shortening
+
+# VERSION
+
+version 1.01
+
+[![Build Status](https://travis-ci.org/gryphonshafer/Bot-IRC-X-WwwShorten.svg)](https://travis-ci.org/gryphonshafer/Bot-IRC-X-WwwShorten)
+[![Coverage Status](https://coveralls.io/repos/gryphonshafer/Bot-IRC-X-WwwShorten/badge.png)](https://coveralls.io/r/gryphonshafer/Bot-IRC-X-WwwShorten)
+
+# SYNOPSIS
+
+    use Bot::IRC;
+
+    Bot::IRC->new(
+        connect => { server => 'irc.perl.org' },
+        plugins => ['WwwShorten'],
+    )->run;
+
+# DESCRIPTION
+
+This [Bot::IRC](https://metacpan.org/pod/Bot::IRC) plugin for automatic URL shortening. It uses
+[TinyURL](http://tinyurl.com) for shortening through [WWW::Shorten](https://metacpan.org/pod/WWW::Shorten).
+
+    <user> bot tiny http://perl.org
+    <bot> user: http://tinyurl.com/9om78
+
+# SEE ALSO
+
+You can look for additional information at:
+
+- [Bot::IRC](https://metacpan.org/pod/Bot::IRC)
+- [GitHub](https://github.com/gryphonshafer/Bot-IRC-X-WwwShorten)
+- [CPAN](http://search.cpan.org/dist/Bot-IRC-X-WwwShorten)
+- [MetaCPAN](https://metacpan.org/pod/Bot::IRC::X::WwwShorten)
+- [AnnoCPAN](http://annocpan.org/dist/Bot-IRC-X-WwwShorten)
+- [Travis CI](https://travis-ci.org/gryphonshafer/Bot-IRC-X-WwwShorten)
+- [Coveralls](https://coveralls.io/r/gryphonshafer/Bot-IRC-X-WwwShorten)
+- [CPANTS](http://cpants.cpanauthors.org/dist/Bot-IRC-X-WwwShorten)
+- [CPAN Testers](http://www.cpantesters.org/distro/T/Bot-IRC-X-WwwShorten.html)
+
+# AUTHOR
+
+Gryphon Shafer <gryphon@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2016 by Gryphon Shafer.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
---
+++ lib/Bot/IRC/X/WwwShorten.pm
@@ -0,0 +1,76 @@
+package Bot::IRC::X::WwwShorten;
+# ABSTRACT: Bot::IRC plugin for automatic URL shortening
+
+use strict;
+use warnings;
+
+use WWW::Shorten qw( TinyURL makeashorterlink );
+
+# VERSION
+
+sub init {
+    my ($bot) = @_;
+
+    $bot->hook(
+        {
+            to_me => 1,
+            text  => qr/\btiny\s+(?<url>\S+)/i,
+        },
+        sub {
+            my ( $bot, $in, $m ) = @_;
+            $bot->reply_to( makeashorterlink( $m->{url} ) );
+        },
+    );
+
+    $bot->helps( 'tiny' => 'Shorten URLs. Usage: <bot nick> tiny <url>.' );
+}
+
+1;
+__END__
+=pod
+
+=begin :badges
+
+=for markdown
+[![Build Status](https://travis-ci.org/gryphonshafer/Bot-IRC-X-WwwShorten.svg)](https://travis-ci.org/gryphonshafer/Bot-IRC-X-WwwShorten)
+[![Coverage Status](https://coveralls.io/repos/gryphonshafer/Bot-IRC-X-WwwShorten/badge.png)](https://coveralls.io/r/gryphonshafer/Bot-IRC-X-WwwShorten)
+
+=end :badges
+
+=for test_synopsis BEGIN { die "SKIP: Bot::IRC may or may not be available." }
+
+=head1 SYNOPSIS
+
+    use Bot::IRC;
+
+    Bot::IRC->new(
+        connect => { server => 'irc.perl.org' },
+        plugins => ['WwwShorten'],
+    )->run;
+
+=head1 DESCRIPTION
+
+This L<Bot::IRC> plugin for automatic URL shortening. It uses
+L<TinyURL|http://tinyurl.com> for shortening through L<WWW::Shorten>.
+
+    <user> bot tiny http://perl.org
+    <bot> user: http://tinyurl.com/9om78
+
+=head1 SEE ALSO
+
+You can look for additional information at:
+
+=for :list
+* L<Bot::IRC>
+* L<GitHub|https://github.com/gryphonshafer/Bot-IRC-X-WwwShorten>
+* L<CPAN|http://search.cpan.org/dist/Bot-IRC-X-WwwShorten>
+* L<MetaCPAN|https://metacpan.org/pod/Bot::IRC::X::WwwShorten>
+* L<AnnoCPAN|http://annocpan.org/dist/Bot-IRC-X-WwwShorten>
+* L<Travis CI|https://travis-ci.org/gryphonshafer/Bot-IRC-X-WwwShorten>
+* L<Coveralls|https://coveralls.io/r/gryphonshafer/Bot-IRC-X-WwwShorten>
+* L<CPANTS|http://cpants.cpanauthors.org/dist/Bot-IRC-X-WwwShorten>
+* L<CPAN Testers|http://www.cpantesters.org/distro/T/Bot-IRC-X-WwwShorten.html>
+
+=for Pod::Coverage init
+
+=cut
